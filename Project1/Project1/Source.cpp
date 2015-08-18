18a3f65df0bd3ea143dd7e1bf0c2b02fd0f70c9a
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2015-08-18 18:06:02 -0500

    readd
    a

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+	bool potato = 1;
+	if (potato != false)
+	{	
+		std::cout << "Worship the potato" << std::endl;
+		std::cin.get();
+		return 0;
+	}
+	else
+	{	
+		std::cout << "commit suicide" << std::endl;
+		std::cin.get();
+		return 0;
+	}
+}
