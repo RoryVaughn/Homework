ac8cd5882d38bfa89b2f07e95e85f19594caa9bc
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2015-09-14 04:06:31 -0500

    Almost done
    grrr

--- Wumpus Assesment/Wumpus Assesment/Wumpus Assesment/Wumpus.cpp
+++ Wumpus Assesment/Wumpus Assesment/Wumpus Assesment/Wumpus.cpp
@@ -11,23 +11,46 @@ void MakeGrid()
 	{
 		for (int h = 0; h < 4; h++)
 		{
-
-			cout << "x = " << i << " y = " << h << endl;
-
+			
 		}
-		cout << endl;
+		
 	}
 }
 
 
 int main()
-{
-	MakeGrid(); //the fuction uses nested loops and an array to creat a 2D grid.
+{ 
+	cout << "Welcome to Wumpus World!\n";
+	cout << "Controls : \n8: Face North, 2: Face South, 4: Face West, \n";
+	cout << "6: Face East, 5: Shoot the arrow, \n0: Move in the direction the character is facing. \n \n" << endl;
+	cout << "Your journey for gold begins in the square (0,0), facing North.\n " << endl; 
 
+		MakeGrid(); //the fuction uses nested loops and an array to create a 2D grid.
+		int start = 1;
+		switch(start)
+			case 1:
+		{
 
-
-
+			{Robot Robot(0, 0, 8);
+			Robot.Gameplay();
+			}
+		}
+		int retry;
+		cout << "enter 1 to retry or anything else to quit." << endl;
+		cin >> retry;
+		switch (retry)
+		{
+		case 1:
+		{
+			{system("cls");
+				return main(); }
+		}
+		default:
+		{return 0; }
+	
+		}
 	system("pause"); //this line of code allows the console to stay open after the game is over.
+	
 	return 0;
 }
 
--- Wumpus Assesment/Wumpus Assesment/Wumpus Assesment/Wumpus.h
+++ Wumpus Assesment/Wumpus Assesment/Wumpus Assesment/Wumpus.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef WUMPUS_H
+#define WUMPUS_H
 
 #include <iostream>
 #include <string>
@@ -8,6 +9,181 @@ struct Player
 {
 	string name;
 	bool Alive;
-	int pos (int x, int y);
+	int Pos_X = 0;
+	int Pos_Y = 0;
+	int Direction;
+
+};
+struct Object
+{
+	string name;
+	bool Exist;
+	int E_Pos_X;
+	int E_Pos_Y;
 	
 };
+class Robot
+{
+public:
+	Robot(int, int, int);
+	void Gameplay();
+
+private:
+	int Pos_X;
+	int Pos_Y;
+	int Direction;
+};
+Robot::Robot(int x, int y, int d)
+{
+	int Pos_X = x;
+	int Pos_Y = y;
+	int Direction = d;
+}
+
+void Robot::Gameplay()
+{
+	Player Robot;
+	Robot.Alive = true;
+	Robot.Pos_X = 0;
+	Robot.Pos_Y = 0;
+	Robot.Direction = 8;
+	Object Pit;
+	Pit.Exist = true;
+	Pit.E_Pos_X = 1;
+	Pit.E_Pos_Y = 3;
+	
+	Object Pit2;
+	Pit2.Exist = true;
+	Pit2.E_Pos_X = 3;
+	Pit2.E_Pos_Y = 1;
+	
+	Object Wumpus;
+	Wumpus.Exist = true;
+	Wumpus.E_Pos_X = 2;
+	Wumpus.E_Pos_Y = 1;
+	Object Gold;
+	Gold.Exist = true;
+	Gold.E_Pos_X = 3;
+	Gold.E_Pos_Y = 3;
+
+	int input;
+	cin >> input;
+	
+	switch (input)
+	{
+	case 8:
+	{
+		cout << "You face North." << endl;
+		Robot.Direction = 8;
+		break;
+	}
+	case 2:
+	{
+		cout << "You face South." << endl;
+		Robot.Direction = 2;
+		break;
+	}
+	case 4:
+	{
+		cout << "You face West." << endl;
+		Robot.Direction = 4;
+		break;
+	}
+	case 6:
+	{
+		cout << "You face East." << endl;
+		Robot.Direction = 6;
+		break;
+	}
+	case 5:
+	{
+		int arrow = 1;
+
+		if( arrow == 1)
+		{ 
+		cout << "You shoot the arrow." << endl;
+		arrow = (arrow - 1);
+		if (Robot.Pos_X == 1)
+		{
+			if (Robot.Pos_Y == 1)
+			{
+				cout << "You killed the Wumpus" << endl;
+				Wumpus.Exist = false;
+			}
+		
+		}
+		else if (Robot.Pos_X == 3)
+		{
+			if (Robot.Pos_Y == 0)
+			{
+				cout << "You killed the Wumpus" << endl;
+				Wumpus.Exist = false;
+			}
+
+		}
+		else if (Robot.Pos_X == 2)
+		{
+			if (Robot.Pos_Y == 3)
+			{
+				
+				if (Robot.Direction = 8)
+				{
+					cout << "You killed the Wumpus" << endl;
+					Wumpus.Exist = false;
+				}
+			}
+
+		}
+		else
+		{
+			cout << "Your arrow misses." << endl;
+		}
+		}
+		else
+		{
+			cout << "You have no more arrows." << endl;
+		}
+
+			break;
+	}
+	default:
+	{
+		cout << "Incorrect input \nPlease enter a number listed in the controls." << endl;
+		return  Robot::Gameplay();
+	}
+	}
+
+
+
+
+	if (Robot.Pos_X == 1)
+	{
+		if (Robot.Pos_Y == 3)
+		{
+			cout << "You Died!" << endl;
+
+		}
+
+	}
+	if (Robot.Pos_X == 2)
+	{
+		if (Robot.Pos_Y == 1)
+		{
+			cout << "You Died!" << endl;
+
+		}
+
+	}
+
+	if (Robot.Pos_X == 3)
+	{
+		if (Robot.Pos_Y == 1)
+		{
+			cout << "You Died!" << endl;
+			
+		}
+		
+	}
+}
+
+#endif WUMPUS_H
