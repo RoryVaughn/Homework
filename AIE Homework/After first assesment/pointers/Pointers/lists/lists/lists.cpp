71f0cd8fbe0fc2cfe5c266f3d43106cd94524e94
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2015-09-15 17:52:58 -0500

    Tuesday commit

---
+++ AIE Homework/After first assesment/pointers/Pointers/lists/lists/lists.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//Rory Vaughn
+//lists
+//Teacher: Matthew Williamson
+//9/15/2015
+using namespace std;
+
+//test on differnce between linked list and arrays
+
+int main()
+{
+	struct node
+	{
+		node* next;
+		int data;
+
+	};
+	
+	//I have no fucking clue what i am doing right now please help me if you 
+
+
+
+	system("pause");
+	return 0;
+}
